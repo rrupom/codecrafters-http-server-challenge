59c74d54ba18292779e24de2ce0d4045b75972f7
Rupom <rakib.t.rupom@gmail.com>
2025-04-23 00:02:17 +0600

    modified server file

--- app/main.js
+++ app/main.js
@@ -5,6 +5,7 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket.write("HTTP/1.1 200 OK\n\r\n\r");
   socket.on("close", () => {
     socket.end();
   });
