ed6f1b09b65d405ce560deefb1b58a29dde250b8
Rupom <rakib.t.rupom@gmail.com>
2025-04-23 00:36:34 +0600

    different response for different path

--- app/main.js
+++ app/main.js
@@ -5,7 +5,16 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.on("data", (data) => {
+    const request = data.toString();
+    const requestLine = request.split("\r\n")[0];
+    const [method, path] = requestLine.split(" ");
+    if (path === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+  });
   socket.on("close", () => {
     socket.end();
   });
