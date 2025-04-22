414eb3ab70dfbd76dbece9c7d37f3ce30f41b2fe
Rupom <rakib.t.rupom@gmail.com>
2025-04-23 01:05:51 +0600

    modified code

--- app/main.js
+++ app/main.js
@@ -6,12 +6,16 @@ const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
     const requestLine = request.split("\r\n")[0];
-    const [method, path] = requestLine.split(" ");
-    const content = path.split("/");
-    const dynamicPart = content[content.length - 1];
-    socket.write(
-      `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${dynamicPart.length}\r\n\r\n${dynamicPart}`
-    );
+    const [_method, path] = requestLine.split(" ");
+    if (path === "/") socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    else if (path.includes("/echo/")) {
+      const dynamicPart = path.split("/echo/")[1];
+      socket.write(
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${dynamicPart.length}\r\n\r\n${dynamicPart}`
+      );
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
   });
   socket.on("close", () => {
     socket.end();
